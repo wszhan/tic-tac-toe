d83ed3cbceac42a5da7389d0de977696fdf29756
wszhan <wszhan@@users.noreply.github.com>
2023-02-04 12:13:33 -0500

    add logic to grid click event

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let isPlayerStrawberrysTurn = true;
+let isPlayerStrawberrysTurn = undefined;
 let winner = undefined;
 
-const STRAWBERRY_ICON_PATH = './assets/icons/icons8-blueberry-color-96.png';
-const BLUEBERRY_ICON_PATH = './assets/icons/icons8-strawberry-color-96.png';
+const STRAWBERRY_ICON_PATH = './assets/icons/icons8-strawberry-color-96.png';
+const BLUEBERRY_ICON_PATH = './assets/icons/icons8-blueberry-color-96.png';
 const STRAWBERRY_PLAYER_NAME = 'strawberry';
 const BLUEBERRY_PLAYER_NAME = 'blueberry';
 const START_MSG = 'Click any grid to start the game';
@@ -12,23 +12,65 @@ const HINT = `Player ${isPlayerStrawberrysTurn ? 'Strawberry' : 'Blueberry'}'s t
 const gameboard = document.querySelector('div.gameboard');
 const grids = [];
 
-function makeGrid() {
+function makeGrid(index) {
     let gridOwner = undefined;
     return {
         setPlayer: player => gridOwner = player,
-        DOMElement: createGridInDom(),
+        DOMElement: createGridInDom(index),
     };
 }
 
+function addClickCallback(gridElement) {
+    gridElement.addEventListener('click', e => {
+        const currDivElement = e.target; // this might not be the div we want, e.g. it can be an image
+        // 1. is this a div.grid-element?
+        // 2. ts this grid occupied?
+        if (!currDivElement.parentNode || currDivElement.parentNode !== gameboard) {
+            return;
+        }
+        const divElementID = currDivElement.id;
+        const gridObject = grids[divElementID];
+        if (gridObject.DOMElement === currDivElement && gridObject.player) {
+            return;
+        }
+
+        // all following is only valid in an empty/unoccupied grid
+        setGridIcon(currDivElement);
+        setGridPlayer(grids[divElementID]);
+
+        swapTurn();
+    });
+}
+
+function setGridPlayer(gridObj, strawberry = isPlayerStrawberrysTurn) {
+    if (gridObj.player) {
+        throw new Error('This grid is occupied.');
+    }
+    gridObj.player = strawberry ? STRAWBERRY_PLAYER_NAME : BLUEBERRY_PLAYER_NAME;
+}
+
+function swapTurn(forceTurn = undefined) {
+    if (forceTurn) {
+        isPlayerStrawberrysTurn = forceTurn;
+        return;
+    }
+    isPlayerStrawberrysTurn = !isPlayerStrawberrysTurn;
+}
+
 function reset() {
+    isPlayerStrawberrysTurn = true;
+
     for (let i = 0; i < 9; i++) {
-        grids.push(makeGrid());
+        const grid = makeGrid(i);
+        grids.push(grid);
+        addClickCallback(grid.DOMElement);
     }
 }
 
-function createGridInDom() {
+function createGridInDom(index) {
     const gridElement = document.createElement('div');
     gridElement.classList.add('gameboard-grid');
+    gridElement.setAttribute('id', index);
     const gridContent = document.createElement('div');
     gridContent.classList.add('grid-content');
     gridElement.appendChild(gridContent);
@@ -36,10 +78,10 @@ function createGridInDom() {
     return gridElement;
 }
 
-function setGridIcon(gridElement, isPlayerStrawberry = true) {
+function setGridIcon(gridElement, strawberry=isPlayerStrawberrysTurn) {
     const gridIcon = document.createElement('img'); 
-    gridIcon.setAttribute('src', isPlayerStrawberry ? STRAWBERRY_ICON_PATH : BLUEBERRY_ICON_PATH);
-    gridElement['DOMElement'].querySelector('div.grid-content').appendChild(gridIcon);
+    gridIcon.setAttribute('src', strawberry ? STRAWBERRY_ICON_PATH : BLUEBERRY_ICON_PATH);
+    gridElement.querySelector('div.grid-content').appendChild(gridIcon);
 }
 
 function init() {
